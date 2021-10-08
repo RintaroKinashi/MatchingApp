67838db00aeabeefc5a3d68cedc5c596255622fe
KinashiRintaro <nagisa624174@gmail.com>
2021-10-08 11:12:45 +0900

    login機能 設定完了

--- routes/web.php
+++ routes/web.php
@@ -18,10 +18,17 @@ VScode x git ：https://qiita.com/y-tsutsu/items/2ba96b16b220fb5913be
 
 laravel 初期セットアップ：https://qiita.com/schrosis/items/cce304870fcb27498a5e
 
-Laravel 日本語化 : https://qiita.com/manbolila/items/498aae00f3574c72f031
+Laravel jetstream : https://qiita.com/manbolila/items/498aae00f3574c72f031
+写真が非表示のときの対処：https://qiita.com/kai0310/items/fc68340dd50df1e1e936
+
+
 
 */
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
